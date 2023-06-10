9764f44efd1d0ef84137c467708c80d2e79b3973
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-06-10 15:07:12 -0300

    Implementado Cliente

---
+++ PartyManager.Dominio/ModuloCliente/Cliente.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PartyManager.Dominio.ModuloCliente
+{
+     public class Cliente : EntidadeBase<Cliente>
+     {
+          public string nome { get; set;}
+          public string telefone;
+
+          public Cliente()
+          {
+
+          }
+
+          public Cliente(string nome, string telefone)
+          {
+               this.nome = nome;
+               this.telefone = telefone;
+          }
+
+          public override void AtualizarRegistros(Cliente registroAtualizado)
+          {
+               nome = registroAtualizado.nome;
+               telefone = registroAtualizado.telefone;
+          }
+
+          public override string[] ValidarErros()
+          {
+               List<string> erros = new List<string>();
+
+               if (string.IsNullOrEmpty(nome))
+                    erros.Add("Campo \"Nome\" está vazio!");
+
+               if (telefone == "(  )     -")
+                    erros.Add("Campo \"Telefone\" está vazio!");
+
+               return erros.ToArray();
+          }
+     }
+}
